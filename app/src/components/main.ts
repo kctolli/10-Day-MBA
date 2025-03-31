893b00a0d71d80c2a06d1a3ab85b8ab58f940fff
kctolli <developer@ktolliver.org>
2025-03-30 20:27:16 -0600

    Deploy --- 03/30/2025 08:27:16 PM

---
+++ app/src/components/main.ts
@@ -0,0 +1,29 @@
+import Day1 from "./Day1";
+import Day2 from "./Day2";
+import Day3 from "./Day3";
+import Day4 from "./Day4";
+import Day5 from "./Day5";
+import Day6 from "./Day6";
+import Day7 from "./Day7";
+import Day8 from "./Day8";
+import Day9 from "./Day9";
+import Day10 from "./Day10";
+
+import Podcasts from "./Podcasts";
+
+const Days = {
+    Day1: Day1,
+    Day2: Day2,
+    Day3: Day3,
+    Day4: Day4,
+    Day5: Day5,
+    Day6: Day6,
+    Day7: Day7,
+    Day8: Day8,
+    Day9: Day9,
+    Day10: Day10
+}
+
+const components = { Days, Podcasts };
+
+export default components;
--- app/src/main.tsx
+++ app/src/main.tsx
@@ -3,8 +3,11 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import App from './App.tsx';
+import Components from './components/main.ts';
 
 import './index.css';
+
+const { Days, Podcasts } = Components;
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
 
@@ -13,6 +16,17 @@ root.render(
     <BrowserRouter basename='/'>
       <Routes>
         <Route index element={<App />}/>
+        <Route path='podcasts' element={<Podcasts />}/>
+        <Route path='day1' element={<Days.Day1 />} />
+        <Route path='day2' element={<Days.Day2 />} />
+        <Route path='day3' element={<Days.Day3 />} />
+        <Route path='day4' element={<Days.Day4 />} />
+        <Route path='day5' element={<Days.Day5 />} />
+        <Route path='day6' element={<Days.Day6 />} />
+        <Route path='day7' element={<Days.Day7 />} />
+        <Route path='day8' element={<Days.Day8 />} />
+        <Route path='day9' element={<Days.Day9 />} />
+        <Route path='day10' element={<Days.Day10 />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
